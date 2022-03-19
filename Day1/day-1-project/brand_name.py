a3f190bf18d5c8258c5367b1779127a02f0bd115
Gourav Kadam <gouravkadam49@gmail.com>
2022-03-19 21:17:38 +0530

    day 1 project

---
+++ Day1/day-1-project/brand_name.py
@@ -0,0 +1,7 @@
+print("welcome!! To Brand name Generator")
+
+city = input("What`s the name of the city you grew up in : ")
+
+name = input("What`s your pet`s name : ")
+
+print("your brand name must be : " + city + " " + name)
