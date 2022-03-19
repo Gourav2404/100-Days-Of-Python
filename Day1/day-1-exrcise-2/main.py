0d7798351b669fea2ebc8488014edaeb00ed8285
Gourav2404 <86721354+Gourav2404@users.noreply.github.com>
2022-03-19 21:02:47 +0530

    day-1 exercise

---
+++ Day1/day-1-exrcise-2/main.py
@@ -0,0 +1,7 @@
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.\n"+'e.g. print("Hello " + "world")\n'+"New lines can be created with a backslash and n.")
+
+
+
